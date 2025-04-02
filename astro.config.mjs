857eaaa7c8747b1891ee0361540e9485ffd2a3d3
varmiguemunoz <varmiguemunoz@gmail.com>
2025-04-02 13:08:07 -0500

    Feature: first website commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import Compress from 'astro-compress';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+site: 'https://www.varmiguemunoz.com/',
+  image: {
+    remotePatterns: [{ protocol: 'https' }],
+  },
+  integrations: [
+    Compress(),
+    sitemap(),
+    tailwind(),
+    robotsTxt(),
+  ],
+});
